06d4dbed474150e678ac470a7c865e3e59e9ec3b
D4nd1ck <alimertkarakaya2001@gmail.com>
2025-12-09 01:52:14 +0300

    Create oyunListesi.js

---
+++ utils/oyunListesi.js
@@ -0,0 +1,17 @@
+module.exports = [
+    "League of Legends",
+    "Valorant",
+    "CS2",
+    "Minecraft",
+    "Fortnite",
+    "GTA 6",
+    "Genshin Impact",
+    "Apex Legends",
+    "PUBG",
+    "Roblox",
+    "Rainbow Six Siege",
+    "EA FC 25",
+    "The Finals",
+    "Helldivers 2",
+    "Overwatch 2"
+];
