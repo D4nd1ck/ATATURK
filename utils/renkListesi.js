cc92a49e23315e031cc7771f14fab5798d1ec9fc
D4nd1ck <alimertkarakaya2001@gmail.com>
2025-12-09 01:52:00 +0300

    Create renkListesi.js

---
+++ utils/renkListesi.js
@@ -0,0 +1,9 @@
+module.exports = [
+    { isim: "Kırmızı", hex: "#ff0000" },
+    { isim: "Mavi", hex: "#3498db" },
+    { isim: "Yeşil", hex: "#2ecc71" },
+    { isim: "Mor", hex: "#9b59b6" },
+    { isim: "Turuncu", hex: "#e67e22" },
+    { isim: "Sarı", hex: "#f1c40f" },
+    { isim: "Pembe", hex: "#ff69b4" }
+];
