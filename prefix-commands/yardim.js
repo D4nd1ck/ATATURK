179de9091d38ab32ca2c8743dfca688ae8577a9c
D4nd1ck <alimertkarakaya2001@gmail.com>
2025-12-09 01:51:44 +0300

    Create yardim.js

---
+++ prefix-commands/yardim.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "yardim",
+
+    async execute(message) {
+        message.reply(`
+📘 **ATATÜRK Bot Yardım**
+\`a!ping\` — Ping ölçer  
+\`/renk\` — Renk komutları  
+\`/oyun\` — Oyun rolleri  
+\`/ses\` — Ses komutları  
+        `);
+    }
+};
