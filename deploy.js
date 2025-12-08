a0a8d2c9b2def14806acff59ee1300d412acb610
D4nd1ck <alimertkarakaya2001@gmail.com>
2025-12-09 01:55:57 +0300

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+    try {
+        console.log(`⚡ Slash komutları ${GUILD_ID} sunucusuna yükleniyor...`);
+
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands }
+        );
+
+        console.log('✅ Slash komutları başarıyla yüklendi!');
+    } catch (error) {
+        console.error(error);
+    }
+})();
