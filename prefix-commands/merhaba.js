ffefafee8d894011a7eb4b9f6b4c1efa2d72cc0b
D4nd1ck <alimertkarakaya2001@gmail.com>
2025-12-09 03:34:57 +0300

    Update merhaba.js

--- prefix-commands/merhaba.js
+++ prefix-commands/merhaba.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "merhaba",
     description: "Belirtilen kanala merhaba mesajı gönderir",
     async execute(message, args, client) {
+        // client burada kullanılabilir
         client.channels.cache.get("KANAL_ID")?.send("Merhaba!");
         message.reply("Mesaj gönderildi!");
     }
